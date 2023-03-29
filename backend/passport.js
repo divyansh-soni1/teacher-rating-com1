caa7f185b9f7a5ada3b79dde40e137b7448a9210
Divyansh soni <97580995+divyansh-soni1@users.noreply.github.com>
2023-03-29 23:42:36 +0530

    Add files via upload

---
+++ backend/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.serializeUser((user , done) => {
+	done(null , user);
+})
+passport.deserializeUser(function(user, done) {
+	done(null, user);
+}); 
+
+passport.use(new GoogleStrategy({
+	clientID:"667724243528-7hb35mp3o49u9b0as6vimhc2gaj94q92.apps.googleusercontent.com", // Your Credentials here.
+	clientSecret:"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", // Your Credentials here.
+	callbackURL:"http://localhost:5000/auth/callback",
+	passReqToCallback:true
+},
+function(request, accessToken, refreshToken, profile, done) {
+	return done(null, profile);
+}
+));
