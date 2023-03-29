2d4642e02dce84822a7bbdedd81214b7c72067e3
Divyansh soni <97580995+divyansh-soni1@users.noreply.github.com>
2023-03-30 00:15:42 +0530

    Update passport.js

--- backend/passport.js
+++ backend/passport.js
@@ -11,7 +11,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
 	clientID:"667724243528-7hb35mp3o49u9b0as6vimhc2gaj94q92.apps.googleusercontent.com", // Your Credentials here.
 	clientSecret:"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", // Your Credentials here.
-	callbackURL:"https://lucky-cheesecake-7d3711.netlify.app/auth/callback",
+	callbackURL:"https://teacher-rate.onrender.com/auth/callback",
 	passReqToCallback:true
 },
 function(request, accessToken, refreshToken, profile, done) {
